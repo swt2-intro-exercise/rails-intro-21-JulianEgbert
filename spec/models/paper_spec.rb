970415620d7970a33182636454e170987828e9ff
JulianEgbert <Julian.Egbert@student.hpi.de>
2021-11-09 15:42:52 +0100

    Failing tests for #13

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
+  it "should accept papers" do
+    @paper = Paper.new(title: "Test", venue: "Testvenue", year: 4711)
+    expect(@paper).to be_valid
+  end
+  
   it "shouldn't accept papers without a title" do
     @paper = Paper.new(venue: "Testvenue", year: 4711)
     expect(@paper).to_not be_valid
@@ -8,6 +13,21 @@ RSpec.describe Paper, type: :model do
 
   it "shouldn't accept papers without any information" do
     @paper = Paper.new()
+    expect(@paper).to_not be_valid
+  end
+
+  it "shouldn't accept papers without a year" do
+    @paper = Paper.new(title: "Test", venue: "Testvenue")
+    expect(@paper).to_not be_valid
+  end
+
+  it "shouldn't accept papers without a venue" do
+    @paper = Paper.new(title: "Test", year: 4711)
+    expect(@paper).to_not be_valid
+  end
+
+  it "should have a numeric year" do
+    @paper = Paper.new(title: "Test", venue: "Testvenue", year: "HelloWorld")
     expect(@paper).to_not be_valid
   end
 end
