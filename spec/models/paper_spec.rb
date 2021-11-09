dd16117ed387f7c19fe1822cb3ff90be67bf4440
JulianEgbert <Julian.Egbert@student.hpi.de>
2021-11-09 15:34:46 +0100

    Failing tests for #12

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "shouldn't accept papers without a title" do
+    @paper = Paper.new(venue: "Testvenue", year: 4711)
+    expect(@paper).to_not be_valid
+  end
+
+  it "shouldn't accept papers without any information" do
+    @paper = Paper.new()
+    expect(@paper).to_not be_valid
+  end
 end
